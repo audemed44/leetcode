826c4c0d33c5bfc8313d5fd0b03b30d70801f17b
audemed44 <aakash1496@gmail.com>
2024-02-10 00:13:32 +0530

    Time: 45 ms (65.86%) | Memory: 21.8 MB (13.02%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -3,7 +3,7 @@ class Solution:
         an_str = "".join(ch.lower() for ch in s if ch.isalnum())
         l = 0
         r = len(an_str)-1
-        while r > l:
+        while r >= l:
             if an_str[l] != an_str[r]:
                 return False
             l += 1
