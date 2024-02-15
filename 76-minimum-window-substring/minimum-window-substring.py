817037e85e8710006a11697e8a20dc8a55dde6d8
audemed44 <aakash1496@gmail.com>
2024-02-16 00:14:30 +0530

    Time: 103 ms (56.45%) | Memory: 17.5 MB (34.06%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,25 +1,23 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        def matches(substr_map,freq_map):
-            for i in substr_map:
-                if i in freq_map and freq_map[i] >= substr_map[i]:
-                    continue
-                else:
-                    return False
-            return True
-
         freq_t = Counter(t)
         l = 0
         minL,minR=-1,-1
         freq_s=defaultdict(int)
         minW = float("infinity")
+        have,need = 0,len(freq_t)
         for r in range(len(s)):
-            freq_s[s[r]] += 1
-            while matches(freq_t,freq_s):
+            ch = s[r]
+            freq_s[ch] += 1
+            if ch in freq_t and freq_s[ch] == freq_t[ch]:
+                have += 1
+            while have==need:
                 if (r-l+1) < minW:
                     minL,minR = l,r+1
                     minW = r-l+1
                 freq_s[s[l]] -= 1
+                if s[l] in freq_t and freq_t[s[l]] > freq_s[s[l]]:
+                    have -= 1
                 l += 1
             r += 1
         return "" if minL==-1 else s[minL:minR]
