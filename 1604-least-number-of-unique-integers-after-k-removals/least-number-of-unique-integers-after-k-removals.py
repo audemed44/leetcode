2c82d60b674828d6555ec016f3037755da6bface
audemed44 <aakash1496@gmail.com>
2024-02-16 19:01:29 +0530

    Time: 353 ms (58.91%) | Memory: 36.7 MB (41.22%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.py
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.py
@@ -1,11 +1,12 @@
 class Solution:
     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
         cnt = Counter(arr)
+        deleted_count = 0
         sorted_cnt = sorted(cnt.items(), key= lambda x: x[1])
         for key, val in sorted_cnt:
             if k >= val:
-                del cnt[key]
+                deleted_count += 1
                 k -= val
             else:
                 break
-        return len(cnt)
+        return len(cnt)-deleted_count
