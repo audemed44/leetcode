10fc5c879c25dc8b33f26df704afde18e1b60121
audemed44 <aakash1496@gmail.com>
2024-03-09 00:56:25 +0530

    Time: 261 ms (40.69%) | Memory: 24 MB (41.49%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -12,7 +12,7 @@ class Solution:
             q = deque()
             q.append((r,c))
             while q:
-                i, j = q.pop()
+                i, j = q.popleft()
                 if (i >= 0 and
                     j >= 0 and
                     i < len(grid) and
