ddc9d0486db3d07dd624836cc34888d477898447
audemed44 <aakash1496@gmail.com>
2024-03-15 22:23:34 +0530

    Time: 120 ms (62.51%) | Memory: 17.9 MB (85.71%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -17,11 +17,15 @@ class Solution:
             dfs(r,c+1)
             dfs(r,c-1)
         for row in range(ROWS):
-            dfs(row,0)
-            dfs(row,COLS-1)
+            if (row,0) in visited or board[row][0] == "O":
+                dfs(row,0)
+            if (row,COLS-1) in visited or board[row][COLS-1] == "O":
+                dfs(row,COLS-1)
         for col in range(COLS):
-            dfs(0,col)
-            dfs(ROWS-1,col)
+            if (0, col) in visited or board[0][col] == "O":
+                dfs(0,col)
+            if (ROWS-1,col) in visited or board[ROWS-1][col] == "O":
+                dfs(ROWS-1,col)
         for row in range(ROWS):
             for col in range(COLS):
                 if (row,col) not in visited:
